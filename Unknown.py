0bf9954a5b83ee9f6d047c12576131cab6985c4d
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-12-12 15:42:37 +0200

    formula printing

--- Unknown.py
+++ Unknown.py
@@ -33,15 +33,23 @@ class formula:
     def highest(self):
         return max(self.degi.keys())
 
+    @property
+    def lowest(self):
+        return min(self.degi.keys())
+
+    def __getitem__(self, d):
+        return self.degi[d]
+
     def __str__(self):
         back = ''
-        for d in range(self.highest)[::-1]:
-            if
-        scalar = '' if self.scalar == 1 else '-' if self.scalar == -1 else str(self.scalar) + (
-            '/' if self.recip else "*")
-        shift = '' if self.shift == 0 else f"+ {self.shift}" if self.shift > 0 else f"- {abs(self.shift)}"
-        return f"{scalar}{self.name} {shift}"
+        for d in range(self.highest, self.lowest-1, -1):
+            if self[d] == 0:
+                continue
+            oper = "" if d == self.highest else "+ " if self[d].scalar >= 0 else "- "
+            back += f"{oper}{self[d]} "
+        return back
 
+"""
     def __add__(self, other):
         if isinstance(other, numbers.Number):
             return formula(self.name, self.shift + other, self.scalar)
@@ -79,5 +87,6 @@ class formula:
 def alg_addition(a: formula, b: formula):
     if a.name == b.name:
         return formula(a.name, a.shift + b.shift, a.scalar + b.scalar)
+"""
 
 #   todo: object of single unknown and it's scalar, and seperete object of one side of the equation.
