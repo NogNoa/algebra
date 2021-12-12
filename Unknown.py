5beb46c4b21eaadabeab096a28cfa2f7c01fd016
NogNoa <53447424+NogNoa@users.noreply.github.com>
2021-12-12 12:31:49 +0200

    arithmatics

--- Unknown.py
+++ Unknown.py
@@ -1,3 +1,7 @@
+import fractions
+import numbers
+
+
 class Unknown:
     def __init__(self, name: str, shift=0, scalar=1):
         self.name = name
@@ -5,6 +9,37 @@ class Unknown:
         self.scalar = scalar
 
     def __str__(self):
-        scalar = self.scalar if self.scalar != 1 else ''
-        shift = f" + {self.shift}" if self.shift > 0 else '- ' + str(self.shift) if self.shift < 0 else ''
-        return f"{scalar}{self.name}{shift}"
+        scalar = '' if self.scalar == 1 else '-' if self.scalar == -1 else self.scalar
+        shift = '' if self.shift == 0 else f"+ {self.shift}" if self.shift > 0 else f"- {abs(self.shift)}"
+        return f"{scalar}{self.name} {shift}"
+
+    def __add__(self, other):
+        if isinstance(other, numbers.Number):
+            return Unknown(self.name, self.shift + other, self.scalar)
+        else:
+            return NotImplemented
+
+    def __mult__(self, other):
+        if isinstance(other, numbers.Number):
+            return Unknown(self.name, self.shift * other, self.scalar * other)
+        else:
+            return NotImplemented
+
+    def __sub__(self, other):
+        if isinstance(other, numbers.Number):
+            return Unknown(self.name, self.shift - other, self.scalar)
+        else:
+            return NotImplemented
+
+    def __rsub__(self, other):
+        if isinstance(other, numbers.Number):
+            return Unknown(self.name, other - self.shift, -self.scalar)
+        else:
+            return NotImplemented
+
+    def __truediv__(self, other):
+        if isinstance(other, numbers.Number):
+            return Unknown(self.name, fractions.Fraction(self.shift, other), fractions.Fraction(self.scalar, other))
+        else:
+            return NotImplemented
+
